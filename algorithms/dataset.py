db8fa5cc5e0b8f9a4cc19558b9ec0cb6f568e6ab
markphan03 <markphan03112003@gmail.com>
2026-03-03 12:30:50 -0600

    Added requirements.txt

--- algorithms/dataset.py
+++ algorithms/dataset.py
@@ -11,14 +11,14 @@ class Game:
         return item == self.model1 or item == self.model2
 
     def get_looser(self) -> Union[str, None]:
-        if self.selected == 'tie' or self.selected == "both_bad":  # Done fixing
+        if self.selected == 'tie':
             return None
         if self.selected == self.model1:
             return self.model2
         return self.model1
 
     def get_winner(self) -> Union[str, None]:
-        if self.selected == 'tie' or self.selected == "both_bad": # Done fixing
+        if self.selected == 'tie':
             return None
         return self.selected
 
